e5e738394468d6ec7703d17a9ed5b43659113fc7
MightyRens <rens@lakens.org>
2019-04-15 20:05:51 +0200

    added like function

--- instagram-bot.py
+++ instagram-bot.py
@@ -37,8 +37,35 @@ class instagramBot:
         not_now_message_element.click()
         time.sleep(3)
 
+    def like_photo(self, hashtag):
+        driver = self.driver
+        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        time.sleep(3)
+        for i in range(1, 3):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+        
+        #search for hashtag link
+        hrefs = driver.find_elements_by_tag_name('a')
+        pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
+        pic_hrefs = [href for href in pic_hrefs if hashtag in href]
+        print(hashtag + " photos: " + str(len(pic_hrefs)))
+
+        for pic_href in pic_hrefs:
+            driver.get(pic_href)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            try:
+                driver.find_element_by_link_text("Vind ik leuk").click()
+                time.sleep(18)
+            except Exception as e:
+                time.sleep(2)
+
 
 rensIG = instagramBot("theworldofnirvana", "qWeQSWTL")
 rensIG.login()
+rensIG.like_photo('kurtcobain')
+
+#hashtags = ['kurtcobain', 'nirvana']
+#[rensIG.like_photo(tag) for tag in hashtags]
 
 # "/accounts/login/?source=auth_switcher"
